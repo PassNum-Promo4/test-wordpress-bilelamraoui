8a9b81ab3a6c5985e31bee68b59058aff3b08595
AMRAOUI-Bilel <33541876+bilelamraoui@users.noreply.github.com>
2018-02-12 20:38:00 +0100

    Update contact-page.php

--- wp-content/themes/wp-bootstrap-starter-child/template-parts/contact-page.php
+++ wp-content/themes/wp-bootstrap-starter-child/template-parts/contact-page.php
@@ -7,7 +7,7 @@ get_header();
 ?>
     <section id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
-					<div class=" w-100 col-12 row ">
+					<div class=" container w-100 col-12 row ">
 				  <div class="row col-12  d-flex justify-content-center " >
 				    <h1 class="text-center col-10 p-5 card" style=" border-color: #000;"  ><?php the_field('titre'); ?></h1>
 				  </div>
@@ -32,11 +32,11 @@ get_header();
 								<?php endif; ?>
 									<p class="m-3">:<?php the_field('github'); ?></p>
 							</div>
-							<div class="row col-lg-6 col-md-12 col-sm-12 d-flex justify-content-start ">
+							<div class=" row col-lg-6 col-md-12 col-sm-12 d-flex justify-content-start ">
 								<?php
 								$image = get_field('mail');
 								if( !empty($image) ): ?>
-									<img class="img-fluid" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+									<img class="img-fluid " src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
 								<?php endif; ?>
 									<p class="m-3">:<?php the_field('email'); ?></p>
 							</div>
@@ -51,32 +51,32 @@ get_header();
 						</div>
 					</div>
 					<div class="col-12 row d-flex justify-content-center ">
-						<form class="col-lg-8 col-md-12 col-sm-12 row  " action="index.html" method="post">
+						<form class="col-lg-8 col-md-12 col-sm-12 row  " action="index.html" method="post" required>
 							<div class="col-12">
 								<h1 class="text-center p-5 card" style=" border-color: #000;" >Contactez Moi</h1>
 							</div>
 							<div class="col-12 row d-flex justify-content-start m-3  ">
 								<label class="col-4">Nom :</label>
-								<input class="col-8 form-control" type="text" name="nom" value="AMRAOUI">
+								<input class="col-8 form-control" type="text" name="nom" value="AMRAOUI" required>
 							</div>
 							<div class="col-12 row d-flex justify-content-start m-3  ">
 								<label for="#prenom" class="col-4" for="">Prenom :</label>
-								<input id="prenom" class="col-8 form-control" type="text" name="prenom" value="bilel">
+								<input id="prenom" class="col-8 form-control" type="text" name="prenom" value="bilel" required>
 							</div>
 							<div class="col-12 row d-flex justify-content-start m-3">
 								<label for="#email" class="col-4" for="">Email :</label>
-								<input id="email" class="col-8 form-control" type="mail" name="mail" value="bilel-amraoui@outlook.fr">
+								<input id="email" class="col-8 form-control" type="mail" name="mail" value="bilel-amraoui@outlook.fr" required>
 							</div>
 							<div class="col-12 row d-flex justify-content-start m-3">
 								<label for="#object" class="col-4" for="">Object :</label>
-								<input id="object" class="col-8 form-control" type="text" name="objet" value="Sujet">
+								<input id="object" class="col-8 form-control" type="text" name="objet" value="Sujet" required>
 							</div>
 							<div class="col-12 row d-flex justify-content-start m-3">
 								<label for="#message" class="col-12" for="">Message :</label>
-							<textarea id="message" class="col-12 form-control" name="message" rows="8" cols="80"></textarea>
+							<textarea id="message" class="col-12 form-control" name="message" rows="8" cols="80" required></textarea>
 							</div>
 							<div class="col-12 row d-flex justify-content-center">
-								<input class="col-5  m-3" type="button" name="button" value="Envoyer">
+								<input class="col-5  m-3" type="submit" name="button" value="Envoyer">
 							</div>
 
 						</form>
@@ -87,3 +87,4 @@ get_header();
 
 <?php
 get_footer();
+
